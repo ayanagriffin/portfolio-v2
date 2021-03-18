008a515f0b3130cbcddb8c735b34cced71d6d885
Ayana Griffin <ayanajg@gmail.com>
2021-03-17 17:49:53 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-62bc8b7c88ef803518a4.js"
+    "url": "webpack-runtime-cff155eb83fd87a5250c.js"
   },
   {
     "url": "framework-8e528b732ab2eaadb7b7.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ed130c923265f25736c00355c41395bd"
+    "revision": "823c6c8ed5441834176ecaaebca82f9a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "6a70127df2dbf2f0e709bc4cc6e4eb82"
+    "revision": "85f84ed935d6b722ce62b25c3325c6aa"
   },
   {
     "url": "polyfill-0eca59d2ee8c0828e3a6.js"
